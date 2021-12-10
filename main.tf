f90553d89ca0be78d61d8a466b6fed0810438415
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-12-10 17:57:51 +0530

    1

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+    project = "airline1-sabre-wolverine"
+    credentials = file("../gke.json")
+}
+resource "google_container_cluster" "primary" {
+  name               = "marcellus-wallace"
+  location           = "us-central1-a"
+  initial_node_count = 3
+  node_config {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    #service_account = google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    labels = {
+      foo = "bar"
+    }
+    tags = ["foo", "bar"]
+  }
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
